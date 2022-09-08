a731e54d8c1829e7421cd0ac1b292b629eb8021b
Maxwell <maxwellkret@gmail.com>
2022-09-07 23:40:38 -0500

    Um Actually

--- UmActuallyScraper.py
+++ UmActuallyScraper.py
@@ -35,10 +35,26 @@ episode_titles = [x for x in all_headers if x not in other_headers]
 master_list = [[[merge_str_list(question_row.contents) for question_row in drop_nl_arr(questions_table.contents)] for questions_table in drop_nl_arr(episode_table.contents[1].contents)] for episode_table in top_div.find_all("table")]
 
 # Add titles and format to dict
+master_dict = {}
+for episode in master_list:
 
+	if ['Statement', 'Answer', 'Corrected'] in episode:
+		episode.remove(['Statement', 'Answer', 'Corrected'])
+	if ['Statement', 'Answer', 'Corrected?'] in episode:
+		episode.remove(['Statement', 'Answer', 'Corrected?'])
 
+	for idx, question in enumerate(episode):
 
+		if len(question) != 3:
+			print("weird q")
+			exit
 
+		episode[idx] = {'Statement': question[0], 'Answer': question[1], 'Corrected': question[2]}
+
+for idx in range(len(episode_titles)):
+	master_dict[episode_titles[idx]] = master_list[idx]
+
+print()
 
 # all_episode_tables = top_div.find_all("table")
 # for episode_table in all_episode_tables:
